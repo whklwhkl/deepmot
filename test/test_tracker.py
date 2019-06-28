7f249ac67efe46e73e415988bdf4344731dbcdde
Hao Wang <phiiipwong@163.com>
2019-06-29 01:35:59 +0800

    use torch instead of numpy

---
+++ test/test_tracker.py
@@ -0,0 +1,41 @@
+import unittest
+import torchvision
+import torch
+import sys
+sys.path.append('.')
+import tracker
+
+
+class T_torch(unittest.TestCase):
+    def setUp(self):
+        self.box1 = torch.rand(3, 4) * 240
+        self.box1[:, 2:] += self.box1[:, :2]
+        self.box2 = torch.rand(6, 4) * 240
+        self.box2[:, 2:] += self.box2[:, :2]
+        self.feature_map = torch.rand(1, 32, 480, 720)
+
+    def test_iou(self):
+        iou = torchvision.ops.box_iou(self.box1, self.box2)
+        print(iou)
+
+    def test_roi(self):
+        roi = torchvision.ops.roi_align(self.feature_map, [self.box1], [3,3])
+        print(roi.shape)
+
+    def test_box_distance(self):
+        dist = tracker.distance(self.box1, self.box2)
+        print('box dist')
+        print(dist)
+
+class T_detector(unittest.TestCase):
+    def setUp(self):
+        return
+
+    def test_box(self):
+        return
+
+    def test_feature(self):
+        return
+
+if __name__ == '__main__':
+    unittest.main()
--- tracker.py
+++ tracker.py
@@ -1,19 +1,54 @@
-import numpy as np
+import torch
 import torch.nn as nn
+from torchvision.ops import box_iou, roi_align
+
+
+def distance(box1, box2):
+    center = lambda box:(box[:,:2] + box[:,2:4])/2
+    center1 = center(box1)
+    center2 = center(box2)
+    delta = center1[:,None] - center2[None]
+    return delta.norm(2, dim=-1)
+
+
+class Object:
+
+    def __init__(self, id:int, box:torch.Tensor, feature:torch.Tensor):
+        self.id = id
+        self.box = box
+        self.feature = feature
+
+
+class Detector:
+
+    def __init__(self, model:nn.Module):
+        self.feature_map = None
+        self.model = model
+
+        def get(module, input, output):
+            self.feature_map = output
+
+        self.model.register_forward_hook(get)   # locale some layer
+
+    def detection(self, img):
+        box = img
+        return box, roi_pooling(self.feature_map, box)
 
 
 class Track:
 
     CURRENT_ID = 0
     HEALTH = 5
+    PROBATION = 3
 
-    def __init__(self, init_box:np.ndarray):
+    def __init__(self, init_box:torch.Tensor):
         self.box = init_box
         self.age = 0
         self.health = Track.HEALTH
         self.is_visible = True
-        self.velocity_2d = np.zeros(4)
-        self.velocity_3d = np.zeros(3)  # todo: estimate this using 2d info
+        self.is_candidate = True
+        self.velocity_2d = torch.zeros(4)
+        self.velocity_3d = torch.zeros(3)  # todo: estimate this using 2d info
         self.id = Track.CURRENT_ID
         Track.CURRENT_ID += 1
 
@@ -21,15 +56,20 @@ class Track:
         pred_box = self.box + self.velocity_2d
         return pred_box
 
-    def update(self, new_box:np.ndarray):
+    def update(self, new_box:torch.Tensor):
         self.age += 1
         self.health = Track.HEALTH
         self.velocity_2d = new_box - self.box
         self.box = new_box
+        self.is_candidate = self.age < Track.PROBATION
 
     def update_null(self):
         self.age += 1
-        self.health -= 1
+        if self.is_candidate:
+            self.health = -9999
+        else:
+            self.health -= 1
+        self.is_visible = False
         self.box = self.predict_box()
 
     def is_dead(self):
@@ -38,19 +78,21 @@ class Track:
 
 class Tracker:
 
-    CUT_OFF_IOU = .5
     BIRTH_IOU = .5
+    CANDIDATE_IOU = .55
     MINIMUM_CONFIDENCE = .6
 
     def __init__(self, model:nn.Module):
         self.tracks = {}    # type: {str:Track}
         self.model = model
 
-    def update(self, boxes:[np.ndarray]):
+    def update(self, boxes:[torch.Tensor]):
         ids, anchors = self._gather()
-        iou_matrix = iou(anchors, boxes)    # type: np.ndarray
-        distance_matrix = 1 - iou_matrix + 12
-        trackers2boxes = self._assign(distance_matrix)
+        iou_matrix = box_iou(anchors, boxes)    # type: torch.Tensor
+        iou_matrix[iou_matrix < Tracker.CANDIDATE_IOU] += 1e5
+        distance_matrix = 1 - iou_matrix + distance(anchors, boxes)
+        # todo: add appearance distance
+        trackers2boxes = self._assign(distance_matrix/2)
         unassigned_box_idx = set(range(iou_matrix.shape[1]))
 
         for track_id, box_index in zip(ids, trackers2boxes):
@@ -73,15 +115,14 @@ class Tracker:
         for id, track in self.tracks.items():
             ids += [id]
             anchors += [track.box]
-        return ids, np.stack(anchors)
+        return ids, torch.stack(anchors)
 
-    def _assign(self, distance_matrix:np.ndarray):
-        trackers2boxes = self.model(distance_matrix)
-        trackers2boxes_ = np.concatenate([trackers2boxes,
-                                          Tracker.MINIMUM_CONFIDENCE * np.ones([trackers2boxes.shape[0], 1])],
-                                         -1)
-        trackers2boxes_ /= np.linalg.norm(trackers2boxes_, 1)
-        return np.argmax(trackers2boxes_, -1)
+    def _assign(self, distance_matrix:torch.Tensor):
+        trackers2boxes = self.model(distance_matrix)        # scores
+        trackers2boxes_ = torch.cat([trackers2boxes,
+                                     Tracker.MINIMUM_CONFIDENCE * torch.ones([trackers2boxes.shape[0], 1])],
+                                    -1)
+        return torch.argmax(trackers2boxes_, -1)
 
     def _birth(self, box):
         new = Track(box)
@@ -89,30 +130,6 @@ class Tracker:
 
     def _kill(self, track_id:str):
         del self.tracks[track_id]
-
-
-class Object:
-
-    def __init__(self, id:int, box:np.ndarray, feature:np.ndarray):
-        self.id = id
-        self.box = box
-        self.feature = feature
-
-
-class Detector:
-
-    def __init__(self, model:nn.Module):
-        self.feature_map = None
-        self.model = model
-
-        def get(module, input, output):
-            self.feature_map = output
-
-        self.model.register_forward_hook(get)
-
-    def detection(self, img):
-        box = img
-        return box, roi_pooling(self.feature_map, box)
 
 
 class Curator:
@@ -125,16 +142,3 @@ class Curator:
 
     def update(self, new_objects:[Object]):
         self.objects = new_objects
-
-
-def iou(box1:np.ndarray, box2:np.ndarray):
-    area = lambda box: (box[:, 2] - box[:, 0]) * (box[:, 3] - box[:, 1])
-    upper_left = np.maximum(box1[:, :2], box2[:, :2])
-    bottom_right = np.minimum(box1[:, 2:4], box2[:, 2:4])
-    intersection = np.maximum(0, area(np.concatenate([upper_left, bottom_right], -1)))
-    union = area(box1) + area(box2) - intersection
-    return intersection / union
-
-
-def roi_pooling(feature_map, box):
-    return feature_map[box]
